7e16f06f4767dd6b223c1c8ad4231ccbf8fa747c
Joel Fright <joelwfright@gmail.com>
2021-03-30 15:42:26 +0100

    Cleaned up code and added comments

--- src/main/java/org/sparta/joel/App.java
+++ src/main/java/org/sparta/joel/App.java
@@ -9,10 +9,12 @@ public class App {
     public static String raindrop(int num){
         String result = "";
 
+        //Checks for numbers that are out of bounds
         if(num <= 0){
             return "Number out of bounds";
         }
 
+        // Checks all factors of int num
         if(num % 3 == 0){
             result += "Pling";
         }
@@ -22,6 +24,8 @@ public class App {
         if(num % 7 == 0){
             result += "Plong";
         }
+
+        //Returns original number if no factors found
         if(result.equals("")){
             return String.valueOf(num);
         }
--- src/test/java/org/sparta/joel/AppTest.java
+++ src/test/java/org/sparta/joel/AppTest.java
@@ -1,7 +1,5 @@
 package org.sparta.joel;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
@@ -11,31 +9,32 @@ public class AppTest {
 
     @Test
     @DisplayName("28 Factor | Plong")
-    public void PlongFactor() {
+    public void plongFactor() {
         Assert.assertEquals("Plong",App.raindrop(28));
     }
 
     @Test
     @DisplayName("30 Factor | PlingPlang")
-    public void PlangFactor() {
+    public void plangFactor() {
         Assert.assertEquals("PlingPlang",App.raindrop(30));
     }
 
     @Test
     @DisplayName("34 Factor | 34")
-    public void NumberFactor() {
+    public void numberFactor() {
         Assert.assertEquals("34",App.raindrop(34));
     }
 
     @Test
     @DisplayName("Out of Bounds | Number out of bounds")
-    public void OutOfBoundsZero(){
+    public void outOfBoundsZero(){
         Assert.assertEquals("Number out of bounds", App.raindrop(0));
     }
 
     @Test
     @DisplayName("Out of Bounds | Number out of bounds")
-    public void OutOfBoundsNegative(){
+    public void outOfBoundsNegative(){
         Assert.assertEquals("Number out of bounds", App.raindrop(-1));
     }
+
 }
