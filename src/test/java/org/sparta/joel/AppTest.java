c5987d98550832963e2a36d9c73867bd60e6ff44
Joel Fright <joelwfright@gmail.com>
2021-03-30 15:30:43 +0100

    Added tests for examples in text document

--- src/test/java/org/sparta/joel/AppTest.java
+++ src/test/java/org/sparta/joel/AppTest.java
@@ -2,13 +2,28 @@ package org.sparta.joel;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 
 public class AppTest {
 
     @Test
-    public void shouldAnswerWithTrue() {
+    @DisplayName("28 Factor | Plong")
+    public void PlongFactor() {
+        Assert.assertEquals("Plong",App.raindrop(28));
+    }
 
+    @Test
+    @DisplayName("30 Factor | PlingPlang")
+    public void PlangFactor() {
+        Assert.assertEquals("PlingPlang",App.raindrop(30));
+    }
+
+    @Test
+    @DisplayName("34 Factor | 34")
+    public void NumberFactor() {
+        Assert.assertEquals("34",App.raindrop(34));
     }
 }
