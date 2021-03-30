3fac032479085900d0ba945b21daa2b61827241f
Joel Fright <joelwfright@gmail.com>
2021-03-30 15:26:21 +0100

    Basic functionality implemented

--- src/main/java/org/sparta/joel/App.java
+++ src/main/java/org/sparta/joel/App.java
@@ -3,7 +3,26 @@ package org.sparta.joel;
 public class App {
 
     public static void main(String[] args) {
+        System.out.println(raindrop(34));
+    }
 
+    public static String raindrop(int num){
+        String result = "";
+
+        if(num % 3 == 0){
+            result += "Pling";
+        }
+        if(num % 5 == 0){
+            result += "Plang";
+        }
+        if(num % 7 == 0){
+            result += "Plong";
+        }
+        if(result.equals("")){
+            return String.valueOf(num);
+        }
+
+        return result;
     }
 
 }
